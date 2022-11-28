fe8ae71ab109dc84901a55c604cf9eb0070cd153
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-11-28 13:20:07 -0700

    commit tree 8a25efd02b256e7467851d8ea064e7dabddd3750

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,26 @@
 class StudentsController < ApplicationController
 
+  # def index
+  #   if params[:name]
+  #     students = Student.by_name(params[:name])
+  #   else
+  #     students = Student.all
+  #   end
+  #   render json: students
+  # end
+
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
